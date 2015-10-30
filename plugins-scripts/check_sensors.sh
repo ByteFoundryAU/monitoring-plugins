b2bc71ac44eb5500f48b729eafe89e3e33c9d8d3
Dean Hamstead <dean@fragfest.com.au>
2015-10-31 00:31:54 +1100

    this handles freebsd acpi temperatures (just tz0)

--- plugins-scripts/check_sensors.sh
+++ plugins-scripts/check_sensors.sh
@@ -23,6 +23,72 @@ print_help() {
 	exit $STATE_OK
 }
 
+linux_sensors() {
+	sensordata=`sensors 2>&1`
+	status=$?
+	if test ${status} -eq 127; then
+		text="SENSORS UNKNOWN - command not found (did you install lmsensors?)"
+		exit=$STATE_UNKNOWN
+	elif test ${status} -ne 0; then
+		text="WARNING - sensors returned state $status"
+		exit=$STATE_WARNING
+	elif echo ${sensordata} | egrep ALARM > /dev/null; then
+		text="SENSOR CRITICAL - Sensor alarm detected!"
+		exit=$STATE_CRITICAL
+	elif echo ${sensordata} | egrep FAULT > /dev/null \
+	    && test "$1" != "-i" -a "$1" != "--ignore-fault"; then
+		text="SENSOR UNKNOWN - Sensor reported fault"
+		exit=$STATE_UNKNOWN
+	else
+		text="SENSORS OK"
+		exit=$STATE_OK
+	fi
+
+	echo "$text"
+	if test "$1" = "-v" -o "$1" = "--verbose"; then
+		echo ${sensordata}
+	fi
+	exit $exit
+}
+
+
+freebsd_temperatures() {
+	sensordata=`sysctl -a hw.acpi.thermal.tz0.temperature hw.acpi.thermal.tz0._CRT 2>&1`
+	status=$?
+
+	if test ${status} -eq 127; then
+		text="SYSCTL UNKNOWN - command not found (how is sysctl missing?)"
+		exit=$STATE_UNKNOWN
+	elif test ${status} -ne 0; then
+		text="WARNING - sysctl returned state $status"
+		exit=$STATE_WARNING
+	fi
+
+	if test -n "${sensordata}"; then
+		# hw.acpi.thermal.tz0.temperature: XX.XC
+		temp=`echo $sensordata | awk '{ print $2 }' | sed 's/\.[0-9][CF]$//'`
+		# hw.acpi.thermal.tz0._CRT: XX.XC
+		max=`echo $sensordata | awk '{ print $4 }' | sed 's/\.[0-9][CF]$//'`
+		warn="$(( ($max * 8)/10 ))"
+
+		if test $temp -ge $max; then
+			text="CRITICAL - tz0 exceeding max temp"
+			exit=$STATE_CRITICAL
+		elif test $temp -ge $warn; then
+			text="WARNING- tz0 exceeding 80% max temp"
+			exit=$STATE_WARNING
+		else
+			text="TEMPERATURES OK"
+			exit=$STATE_OK
+		fi
+
+		echo "$text"
+		exit $exit
+	fi
+
+
+}
+
 case "$1" in
 	--help)
 		print_help
@@ -41,30 +107,14 @@ case "$1" in
 		exit $STATE_OK
 		;;
 	*)
-		sensordata=`sensors 2>&1`
-		status=$?
-		if test ${status} -eq 127; then
-			text="SENSORS UNKNOWN - command not found (did you install lmsensors?)"
-			exit=$STATE_UNKNOWN
-		elif test ${status} -ne 0; then
-			text="WARNING - sensors returned state $status"
-			exit=$STATE_WARNING
-		elif echo ${sensordata} | egrep ALARM > /dev/null; then
-			text="SENSOR CRITICAL - Sensor alarm detected!"
-			exit=$STATE_CRITICAL
-		elif echo ${sensordata} | egrep FAULT > /dev/null \
-		    && test "$1" != "-i" -a "$1" != "--ignore-fault"; then
-			text="SENSOR UNKNOWN - Sensor reported fault"
-			exit=$STATE_UNKNOWN
-		else
-			text="SENSORS OK"
-			exit=$STATE_OK
+		os=`uname -o`
+		if echo ${os} | grep Linux > /dev/null; then
+			linux_sensors
 		fi
-
-		echo "$text"
-		if test "$1" = "-v" -o "$1" = "--verbose"; then
-			echo ${sensordata}
+		if echo ${os} | grep FreeBSD > /dev/null; then
+			freebsd_temperatures
 		fi
-		exit $exit
+		echo "Unsupported OS $os"
+		exit $STATE_UNKNOWN
 		;;
 esac
